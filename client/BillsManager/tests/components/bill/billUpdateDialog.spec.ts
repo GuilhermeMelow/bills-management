740f381fbdd45dd17da724939ae7dc35b29353c4
GuilhermeMelow <guilhermefepeme@hotmail.com>
2023-01-09 15:08:37 -0300

    Separando teste do BillUpdateDialog em passos.

--- client/BillsManager/tests/components/bill/billUpdateDialog.spec.ts
+++ client/BillsManager/tests/components/bill/billUpdateDialog.spec.ts
@@ -1,8 +1,8 @@
 import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest'
 import { BillRequest } from '../../../src/types/bill'
+import { fireEvent, render, waitFor } from '@testing-library/vue'
 
 import BillUpdateDialog from '../../../src/components/bill/BillUpdateDialog.vue'
-import { fireEvent, render, waitFor } from '@testing-library/vue'
 
 const mutate = vi.fn((bill: BillRequest) => {
 	return bill
@@ -36,38 +36,74 @@ afterAll(() => {
 })
 
 describe('(BlackBox) BillUpdateDialog.vue', () => {
-	it('x', async () => {
-		const { getByTestId, emitted, debug } = render(BillUpdateDialog, {
+	const setup = () => {
+		return render(BillUpdateDialog, {
 			props: { id: bills[0].id },
 		})
+	}
 
-		await fireEvent.click(getByTestId('button-update'))
-		const getDialog = () => getByTestId('dialog')
-		expect(getDialog).not.toThrowError()
-
+	const getForm = async (
+		getByTestId: <T extends HTMLElement>(id: string) => T
+	) => {
 		await waitFor(() => getByTestId('input-descricao'))
-		const form = {
+
+		return {
 			description: getByTestId<HTMLInputElement>('input-descricao'),
 			price: getByTestId<HTMLInputElement>('input-preco'),
 			dueDate: getByTestId<HTMLInputElement>('input-vencimento'),
 			button: getByTestId<HTMLButtonElement>('button-apply'),
 		}
-		const input = {
-			id: bills[0].id,
-			description: 'Descrição Alterada',
-			dueDate: 30,
-			price: 60,
-		}
-		await Promise.all([
-			fireEvent.update(form.description, input.description),
-			fireEvent.update(form.dueDate, input.dueDate.toString()),
-			fireEvent.update(form.price, input.price.toString()),
-		])
-		await fireEvent.click(form.button)
-		expect(mutate).toHaveBeenCalledTimes(1)
-		expect(mutate).toHaveBeenCalledWith(input)
+	}
 
+	it('Open dialog', async () => {
+		// Arrange
+		const { getByTestId } = setup()
+
+		// Act
 		await fireEvent.click(getByTestId('button-update'))
-		expect(getDialog).toThrowError()
+
+		// Assert
+		expect(() => getByTestId('dialog')).not.toThrowError()
+	})
+
+	it('Form with the value passed', async () => {
+		// Arrange
+		const { getByTestId } = setup()
+		await fireEvent.click(getByTestId('button-update'))
+
+		// Act
+		const form = await getForm(getByTestId)
+
+		// Assert
+		expect(bills[0]).toMatchObject({
+			description: form.description.value,
+			dueDate: form.dueDate.valueAsNumber,
+			price: form.price.valueAsNumber,
+		})
+	})
+
+	it('Mutate should receive updated values from form', async () => {
+		// Arrange
+		const { getByTestId } = setup()
+		await fireEvent.click(getByTestId('button-update'))
+
+		const form = await getForm(getByTestId)
+		await Promise.all([
+			fireEvent.update(form.description, form.description.value),
+			fireEvent.update(form.dueDate, form.dueDate.value),
+			fireEvent.update(form.price, form.price.value),
+		])
+
+		// Act
+		await fireEvent.click(form.button)
+
+		// Assert
+		expect(mutate).toHaveBeenCalledTimes(1)
+		expect(mutate).toHaveBeenCalledWith({
+			id: bills[0].id,
+			description: form.description.value,
+			dueDate: form.dueDate.valueAsNumber,
+			price: form.price.valueAsNumber,
+		})
 	})
 })
